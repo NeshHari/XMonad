406a92d9c20d55c1ab57ae75da1d8968806a7df0
Nesh Hari <neshharipil@gmail.com>
2022-12-29 12:11:32 +0800

    fzf alias

--- fish/.config/fish/functions/parusi.fish
+++ fish/.config/fish/functions/parusi.fish
@@ -1,3 +1,3 @@
-function parusi --wraps=paru\ -Slq\ \|\ fzf\ --multi\ --preview\ \'paru\ -Si\ \{1\}\'\ \|\ xargs\ -ro\ sudo\ paru\ -S --description alias\ parusi=paru\ -Slq\ \|\ fzf\ --multi\ --preview\ \'paru\ -Si\ \{1\}\'\ \|\ xargs\ -ro\ sudo\ paru\ -S
-  paru -Slq | fzf --multi --preview 'paru -Si {1}' | xargs -ro sudo paru -S $argv; 
+function parusi --wraps=paru\ -Slq\ \|\ fzf\ --multi\ --preview\ \'paru\ -Si\ \{1\}\'\ \|\ xargs\ -ro\ sudo\ paru\ -S --wraps=paru\ -Slq\ \|\ fzf\ --multi\ --preview\ \'paru\ -Si\ \{1\}\'\ \|\ xargs\ -ro\ paru\ -S --description alias\ parusi=paru\ -Slq\ \|\ fzf\ --multi\ --preview\ \'paru\ -Si\ \{1\}\'\ \|\ xargs\ -ro\ paru\ -S
+  paru -Slq | fzf --multi --preview 'paru -Si {1}' | xargs -ro paru -S $argv; 
 end
