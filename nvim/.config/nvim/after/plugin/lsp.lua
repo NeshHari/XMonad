6e1d9ca29838829bbcdd676b8e0fc89fbf331dff
Nesh Hari <neshharipil@gmail.com>
2022-12-30 20:23:08 +0800

    lsp diagnostic keybinds

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,3 +2,21 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.setup()
+
+require("mason").setup({
+})
+require("mason-lspconfig").setup({
+})
+
+--local util = require("lspconfig/util")
+
+require'lspconfig'.hls.setup{
+}
+
+
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
