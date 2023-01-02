4f1d3d32356a593a894840537ee3be0ff4e0717e
Nesh Hari <neshharipil@gmail.com>
2023-01-02 15:20:58 +0800

    spotify helper + key&mouse modularisation

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,18 +1,21 @@
+import Custom.MyKeys
 import Custom.MyLayouts
+import Custom.MyMouse
 import Custom.MyWorkspaces
 import Data.Map qualified as M
 import Data.Monoid
 import System.Exit
 import XMonad
-import XMonad (XConfig (startupHook), className, doFloat)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers (doCenterFloat)
+import XMonad.Hooks.OnPropertyChange
 import XMonad.Hooks.StatusBar
 import XMonad.Hooks.StatusBar.PP
 import XMonad.StackSet qualified as W
 import XMonad.Util.SpawnOnce
-import XMonad.Hooks.OnPropertyChange
+import XMonad.Util.EZConfig
 
 myTerminal = "kitty"
 
@@ -27,86 +30,6 @@ myNormalBorderColor = "#1e1e2e"
 
 myFocusedBorderColor = "#cba6f7"
 
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
---
-myKeys conf@(XConfig {XMonad.modMask = modm}) =
-  M.fromList $
-    -- launch a terminal
-    [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf),
-      -- launch dmenu
-      ((modm, xK_p), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\""),
-      -- launch gmrun
-      ((modm .|. shiftMask, xK_p), spawn "gmrun"),
-      -- close focused window
-      ((modm .|. shiftMask, xK_c), kill),
-      -- Rotate through the available layout algorithms
-      ((modm, xK_space), sendMessage NextLayout),
-      --  Reset the layouts on the current workspace to default
-      ((modm .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf),
-      -- Resize viewed windows to the correct size
-      ((modm, xK_n), refresh),
-      -- Move focus to the next window
-      ((modm, xK_Tab), windows W.focusDown),
-      -- Move focus to the next window
-      ((modm, xK_j), windows W.focusDown),
-      -- Move focus to the previous window
-      ((modm, xK_k), windows W.focusUp),
-      -- Move focus to the master window
-      ((modm, xK_m), windows W.focusMaster),
-      -- Swap the focused window and the master window
-      ((modm, xK_Return), windows W.swapMaster),
-      -- Swap the focused window with the next window
-      ((modm .|. shiftMask, xK_j), windows W.swapDown),
-      -- Swap the focused window with the previous window
-      ((modm .|. shiftMask, xK_k), windows W.swapUp),
-      -- Shrink the master area
-      ((modm, xK_h), sendMessage Shrink),
-      -- Expand the master area
-      ((modm, xK_l), sendMessage Expand),
-      -- Push window back into tiling
-      ((modm, xK_t), withFocused $ windows . W.sink),
-      -- Increment the number of windows in the master area
-      ((modm, xK_comma), sendMessage (IncMasterN 1)),
-      -- Deincrement the number of windows in the master area
-      ((modm, xK_period), sendMessage (IncMasterN (-1))),
-      -- Quit xmonad
-      ((modm .|. shiftMask, xK_q), io (exitWith ExitSuccess)),
-      -- Restart xmonad
-      ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart")
-    ]
-      ++ [ ((m .|. modm, k), windows $ f i)
-           | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9],
-             (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-         ]
-      ++ [ ((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-           | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..],
-             (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
-         ]
-
-myMouseBindings (XConfig {XMonad.modMask = modm}) =
-  M.fromList $
-    -- mod-button1, Set the window to floating mode and move by dragging
-    [ ( (modm, button1),
-        ( \w ->
-            focus w
-              >> mouseMoveWindow w
-              >> windows W.shiftMaster
-        )
-      ),
-      -- mod-button2, Raise the window to the top of the stack
-      ((modm, button2), (\w -> focus w >> windows W.shiftMaster)),
-      -- mod-button3, Set the window to floating mode and resize by dragging
-      ( (modm, button3),
-        ( \w ->
-            focus w
-              >> mouseResizeWindow w
-              >> windows W.shiftMaster
-        )
-      )
-      -- you may also bind events to the mouse scroll wheel (button4 and button5)
-    ]
-
 myManageHook =
   composeAll
     [ className =? "MPlayer" --> doFloat,
@@ -115,17 +38,21 @@ myManageHook =
       resource =? "kdesktop" --> doIgnore
     ]
 
-
 myStartupHook :: X ()
 myStartupHook = do
   spawn "~/.fehbg"
   spawnOnce "xmodmap ~/.Xmodmap"
   spawn "killall picom; picom -b"
-  spawn "~/feh-blur.sh -s; ~/feh-blur.sh -d" 
+  spawn "~/feh-blur.sh -s; ~/feh-blur.sh -d"
 
 -- spawnOnce "eww daemon"
 -- spawn "eww close-all"
 -- spawn "eww open bar"
+--
+myDynamicManageHook :: ManageHook
+myDynamicManageHook =
+  composeAll
+    []
 
 main :: IO ()
 main =
@@ -144,15 +71,16 @@ main =
         normalBorderColor = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
         -- key bindings
-        keys = myKeys,
+        keys = myAdditionalKeys,
         mouseBindings = myMouseBindings,
         -- hooks, layouts
         layoutHook = myLayoutHook,
         manageHook = myManageHook,
-        handleEventHook = onXPropertyChange "WM_NAME" (title =? "Spotify" --> doShift "2"),
+        handleEventHook = onXPropertyChange "WM_NAME" myDynamicManageHook,
         logHook = dynamicLog,
         startupHook = myStartupHook
       }
+      `additionalKeysP` myKeys
 
 myPolybarConf =
   def
@@ -165,6 +93,5 @@ myPolybarConf =
 
 polybarPPdef =
   def
-    {
-      ppTitle = const ""
+    { ppTitle = const ""
     }
