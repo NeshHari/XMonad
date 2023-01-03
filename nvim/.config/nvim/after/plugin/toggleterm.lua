8f78623f4127df1b9b565ae20542ee14e09cfbbd
Nesh Hari <neshharipil@gmail.com>
2023-01-03 20:49:59 +0800

    basic config && lazygit support

---
+++ nvim/.config/nvim/after/plugin/toggleterm.lua
@@ -0,0 +1,16 @@
+require("toggleterm").setup {
+    open_mapping = [[<c-\>]],
+    size = 25,
+    shell = "fish",
+    hide_numbers = true,
+    close_on_exit = true,
+}
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+    lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
