753f7b3aa70106a39ba4cd072d1041f2f5d57adf
Nesh Hari <neshharipil@gmail.com>
2023-01-05 14:49:23 +0800

    demodularise colors

---
+++ xmonad/.config/xmonad/lib/Custom/MyCatppuccin.hs
@@ -0,0 +1,53 @@
+module Custom.MyCatppuccin where
+
+catRosewater = "#f5e0dc"
+
+catFlamingo = "#f2cdcd"
+
+catPink = "#f5c2e7"
+
+catMauve = "#cba6f7"
+
+catRed = "#f38ba8"
+
+catMaroon = "#eba0ac"
+
+catPeach = "#fab387"
+
+catYellow = "#f9e2af"
+
+catGreen = "#a6e3a1"
+
+catTeal = "#94e2d5"
+
+catSky = "#89dceb"
+
+catSapphire = "#74c7ec"
+
+catBlue = "#89b4fa"
+
+catLavender = "#b4befe"
+
+catText = "#cdd6f4"
+
+catSubtext1 = "#bac2de "
+
+catSubtext0 = "#a6adc8 "
+
+catOverlay2 = "#9399b2 "
+
+catOverlay1 = "#7f849c "
+
+catOverlay0 = "#6c7086 "
+
+catSurface2 = "#585b70 "
+
+catSurface1 = "#45475a"
+
+catSurface0 = "#313244"
+
+catBase = "#1e1e2e"
+
+catMantle = "#181825"
+
+catCrust = "#11111b"
