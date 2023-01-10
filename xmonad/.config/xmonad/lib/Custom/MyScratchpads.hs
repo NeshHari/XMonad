e84974affe516fd8bc83647568fdf36271f58487
Nesh Hari <neshharipil@gmail.com>
2023-01-10 17:05:17 +0800

    positioning enhancements

--- xmonad/.config/xmonad/lib/Custom/MyScratchpads.hs
+++ xmonad/.config/xmonad/lib/Custom/MyScratchpads.hs
@@ -4,17 +4,31 @@ import XMonad
 import XMonad.StackSet qualified as W
 import XMonad.Util.NamedScratchpad
 
+myCenter :: ManageHook
+myCenter = customFloating $ W.RationalRect fromLeft fromTop width height
+  where
+    width = 1 / 2
+    height = 1 / 2
+    fromLeft = (1 - width) / 2
+    fromTop = (1 - height) / 2
+
+myCenterSmall :: ManageHook
+myCenterSmall = customFloating $ W.RationalRect fromLeft fromTop width height
+  where
+    width = 1 / 3
+    height = 1 / 3
+    fromLeft = (1 - width) / 2
+    fromTop = (1 - height) / 2
+
 myScratchpads :: [NamedScratchpad]
 myScratchpads =
-  [ NS "quick commands" spawnQc findQc manageQc,
-    NS "pavucontrol" spawnPavu findPavu managePavu
+  [ NS "quick commands" spawnQc findQc myCenter,
+    NS "glava" spawnGl findGl myCenterSmall
   ]
   where
-    -- emulate "drop-down behavior"
-    spawnQc = "alacritty"
+    -- use different terminal from default
+    spawnQc = "alacritty -e fish"
     findQc = resource =? "Alacritty"
-    manageQc = customFloating $ W.RationalRect 0 0 1 1
 
-    spawnPavu = "pavucontrol"
-    findPavu = resource =? "pavucontrol"
-    managePavu = customFloating $ W.RationalRect (1 / 4) (1 / 4) (1 / 2) (1 / 2)
+    spawnGl = "glava"
+    findGl = resource =? "GLava"
