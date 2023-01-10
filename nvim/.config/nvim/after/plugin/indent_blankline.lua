264ab12e22d0d0316c9305662a7e8be9c19cb5f6
Nesh Hari <neshharipil@gmail.com>
2023-01-10 14:48:49 +0800

    dashboard customisation

--- nvim/.config/nvim/after/plugin/indent_blankline.lua
+++ nvim/.config/nvim/after/plugin/indent_blankline.lua
@@ -1,4 +1,7 @@
 require("indent_blankline").setup {
+    filetype_exclude = {
+        "dashboard"
+    },
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = true,
