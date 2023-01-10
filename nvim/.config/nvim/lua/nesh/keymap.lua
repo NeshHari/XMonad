3e9dcfd1e32265008e951de0a42aa0e6aa616593
Nesh Hari <neshharipil@gmail.com>
2023-01-10 14:48:17 +0800

    refinements

--- nvim/.config/nvim/lua/nesh/keymap.lua
+++ nvim/.config/nvim/lua/nesh/keymap.lua
@@ -5,12 +5,8 @@ vim.g.mapleader = " "
 key.set("n", "<leader>pv", vim.cmd.Ex)
 key.set("v", "J", ":m '>+1<CR>gv=gv")
 key.set("v", "K", ":m '<-2<CR>gv=gv")
-
 key.set("x", "<leader>p", [["_dP]])
-
 key.set({ "n", "v" }, "<leader>y", [["+y]])
 key.set("n", "<leader>Y", [["+Y]])
-
 key.set({ "n", "v" }, "<leader>d", [["_d]])
-
 key.set("n", "<leader>fo", vim.lsp.buf.format)
