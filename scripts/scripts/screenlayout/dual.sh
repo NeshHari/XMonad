9a974795f321431b605d30eba91152c141348c8e
Nesh Hari <neshharipil@gmail.com>
2023-01-11 15:36:29 +0800

    minor tweaks

--- scripts/scripts/screenlayout/dual.sh
+++ scripts/scripts/screenlayout/dual.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 1121x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output USB-C-0 --off
+xrandr --output HDMI-0 --primary --mode 2560x1440 --pos 1121x0 --rotate normal --rate 144 --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate left --rate 60 --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output USB-C-0 --off
+
