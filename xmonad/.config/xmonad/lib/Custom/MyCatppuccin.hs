4d8be0eef6629022d19d5f704b05b0d48fe2b3b7
Nesh Hari <neshharipil@gmail.com>
2023-01-12 14:34:32 +0800

    typecast

--- xmonad/.config/xmonad/lib/Custom/MyCatppuccin.hs
+++ xmonad/.config/xmonad/lib/Custom/MyCatppuccin.hs
@@ -1,28 +1,79 @@
 module Custom.MyCatppuccin where
 
+catRosewater :: String
 catRosewater = "#f5e0dc"
+
+catFlamingo :: String
 catFlamingo = "#f2cdcd"
+
+catPink :: String
 catPink = "#f5c2e7"
+
+catMauve :: String
 catMauve = "#cba6f7"
+
+catRed :: String
 catRed = "#f38ba8"
+
+catMaroon :: String
 catMaroon = "#eba0ac"
+
+catPeach :: String
 catPeach = "#fab387"
+
+catYellow :: String
 catYellow = "#f9e2af"
+
+catGreen :: String
 catGreen = "#a6e3a1"
+
+catTeal :: String
 catTeal = "#94e2d5"
+
+catSky :: String
 catSky = "#89dceb"
+
+catSapphire :: String
 catSapphire = "#74c7ec"
+
+catBlue :: String
 catBlue = "#89b4fa"
+
+catLavender :: String
 catLavender = "#b4befe"
+
+catText :: String
 catText = "#cdd6f4"
+
+catSubtext1 :: String
 catSubtext1 = "#bac2de"
+
+catSubtext0 :: String
 catSubtext0 = "#a6adc8"
+
+catOverlay2 :: String
 catOverlay2 = "#9399b2"
+
+catOverlay1 :: String
 catOverlay1 = "#7f849c"
+
+catOverlay0 :: String
 catOverlay0 = "#6c7086"
+
+catSurface2 :: String
 catSurface2 = "#585b70"
+
+catSurface1 :: String
 catSurface1 = "#45475a"
+
+catSurface0 :: String
 catSurface0 = "#313244"
+
+catBase :: String
 catBase = "#1e1e2e"
+
+catMantle :: String
 catMantle = "#181825"
+
+catCrust :: String
 catCrust = "#11111b"
