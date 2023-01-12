ebd2a766520a34893b10103b4dfc57ee007278ab
Nesh Hari <neshharipil@gmail.com>
2023-01-12 14:40:23 +0800

    new dashboard

--- nvim/.config/nvim/after/plugin/alpha.lua
+++ nvim/.config/nvim/after/plugin/alpha.lua
@@ -1,1 +1,26 @@
-require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+local dashboard = require('alpha.themes.dashboard')
+
+-- Banner
+local banner = {
+    "                                                    ",
+    " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                    ",
+}
+
+dashboard.section.header.val = banner
+
+dashboard.section.buttons.val = {
+    dashboard.button('SPC f f', '  Find File', ':Telescope find_files find_command=rg,--hidden,--files<CR>'),
+    dashboard.button('SPC f b', '  File Browser', ':Telescope file_browser<CR>'),
+    dashboard.button('SPC f w', '  Find Word', ':Telescope live_grep<CR>'),
+    dashboard.button('SPC f d', '  Dotfiles', ':cd $HOME/dotfiles/ | Telescope file_browser<CR>'),
+    dashboard.button('SPC f h', '  Help', ':Telescope help_tags<CR>'),
+    dashboard.button('q', '  Get Me Outta Here', ':qa<CR>'),
+}
+
+require 'alpha'.setup(dashboard.config)
