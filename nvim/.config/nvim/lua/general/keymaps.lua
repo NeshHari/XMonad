c38bfb73214e0b6dd69d0a3dbd331f2e2435cb5c
Nesh Hari <neshharipil@gmail.com>
2023-01-12 14:39:54 +0800

    removed unused mappings

--- nvim/.config/nvim/lua/general/keymaps.lua
+++ nvim/.config/nvim/lua/general/keymaps.lua
@@ -3,10 +3,12 @@ local key = vim.keymap
 vim.g.mapleader = " "
 
 key.set("n", "<leader>pv", vim.cmd.Ex)
+-- move whilst highlighted
 key.set("v", "J", ":m '>+1<CR>gv=gv")
 key.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- paste over
 key.set("x", "<leader>p", [["_dP]])
-key.set({ "n", "v" }, "<leader>y", [["+y]])
-key.set("n", "<leader>Y", [["+Y]])
-key.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- format
 key.set("n", "<leader>fo", vim.lsp.buf.format)
