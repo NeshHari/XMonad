50f56d73a50ddcca8e00b1acc23c0407b798d54f
Nesh Hari <neshharipil@gmail.com>
2023-01-12 14:35:32 +0800

    better prompts

--- xmonad/.config/xmonad/lib/Custom/MyPrompts.hs
+++ xmonad/.config/xmonad/lib/Custom/MyPrompts.hs
@@ -6,17 +6,18 @@ import XMonad.Prompt
 myPromptConfig :: XPConfig
 myPromptConfig =
   def
-    { historySize = 0,
+    { bgColor = catBase,
+      fgColor = catLavender,
+      bgHLight = catLavender,
+      fgHLight = catBase,
+      historySize = 0,
       position = Top,
-      promptBorderWidth = 0,
+      borderColor = catCrust,
+      promptBorderWidth = 6,
       defaultText = "",
       alwaysHighlight = True,
       height = 50,
       font = "xft:Sugar Snow:style=Regular:size=12",
-      bgColor = catBase,
-      fgColor = catLavender,
-      bgHLight = catLavender,
-      fgHLight = catBase,
       autoComplete = Nothing,
       showCompletionOnTab = False
     }
