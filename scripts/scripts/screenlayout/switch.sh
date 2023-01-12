cce5fc4d0414477cd0de3f6d6dd3d21ae367a6ea
Nesh Hari <neshharipil@gmail.com>
2023-01-12 14:37:17 +0800

    minor tweaks

--- scripts/scripts/screenlayout/switch.sh
+++ scripts/scripts/screenlayout/switch.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
-main=HDMI-0
-left=DP-1
-
-if xrandr | grep "$main connected" && xrandr | grep "$left disconnected"; then
-    ~/scripts/screenlayout/single.sh
-fi
-
-if xrandr | grep "$main connected" && xrandr | grep "$left connected"; then
-    ~/scripts/screenlayout/dual.sh
-fi
+##!/bin/sh
+#main=HDMI-0
+#left=DP-1
+#
+#if xrandr | grep "$main connected" && xrandr | grep "$left disconnected"; then
+#    ~/scripts/screenlayout/single.sh
+#fi
+#
+#if xrandr | grep "$main connected" && xrandr | grep "$left connected"; then
+#    ~/scripts/screenlayout/dual.sh
+#fi
