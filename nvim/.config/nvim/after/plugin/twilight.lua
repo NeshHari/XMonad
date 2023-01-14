090866a8df637bec64ffae07324a1af26ca4b128
Nesh Hari <neshharipil@gmail.com>
2023-01-14 12:44:41 +0800

    added defaults

--- nvim/.config/nvim/after/plugin/twilight.lua
+++ nvim/.config/nvim/after/plugin/twilight.lua
@@ -1,1 +1,12 @@
-require("twilight").setup()
+require("twilight").setup
+{
+    dimming = {
+        term_bg = "#000000",
+        inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
+    },
+    context = 10, -- amount of lines we will try to show around the current line
+    treesitter = true, -- use treesitter when available for the filetype
+    -- treesitter is used to automatically expand the visible text,
+    -- but you can further control the types of nodes that should always be fully expanded
+    exclude = {}, -- exclude these filetypes
+}
