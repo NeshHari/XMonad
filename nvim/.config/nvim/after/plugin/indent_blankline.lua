5f00932b2651fee874c57c647606e828ae926ac9
Nesh Hari <neshharipil@gmail.com>
2023-01-14 12:45:13 +0800

    refined indents

--- nvim/.config/nvim/after/plugin/indent_blankline.lua
+++ nvim/.config/nvim/after/plugin/indent_blankline.lua
@@ -2,9 +2,7 @@ require("indent_blankline").setup {
     filetype_exclude = {
         "dashboard"
     },
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
+    char = "",
     char_highlight_list = {
         "IndentBlanklineIndent1",
         "IndentBlanklineIndent2",
@@ -13,4 +11,15 @@ require("indent_blankline").setup {
         "IndentBlanklineIndent5",
         "IndentBlanklineIndent6",
     },
+    space_char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+    },
+    show_trailing_blankline_indent = false,
+    use_treesitter = true,
+    use_treesitter_scope = true,
 }
