c391d1cb0a778ae15f1223dcae13c377411c3666
Nesh Hari <neshharipil@gmail.com>
2023-01-14 12:44:25 +0800

    cleaner zen mode

--- nvim/.config/nvim/after/plugin/zenmode.lua
+++ nvim/.config/nvim/after/plugin/zenmode.lua
@@ -1,7 +1,25 @@
 require("zen-mode").setup {
+    window = {
+        backdrop = 1,
+        width = 50,
+        height = 0.5,
+        options = {
+            signcolumn = "no",
+            number = false,
+            relativenumber = false,
+            cursorline = false,
+            cursorcolumn = false,
+            foldcolumn = "0",
+            list = false,
+        },
+    },
     plugins = {
-        twilight = {
+        options = {
             enabled = true,
-        }
-    }
+            ruler = false,
+            showcmd = false,
+        },
+        twilight = { enabled = false },
+        gitsigns = { enabled = false },
+    },
 }
