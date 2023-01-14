60a8ebcf5a7383ba1effedebec34eb2321b51615
Nesh Hari <neshharipil@gmail.com>
2023-01-14 12:44:55 +0800

    ignore changes

--- nvim/.config/nvim/after/plugin/bufferline.lua
+++ nvim/.config/nvim/after/plugin/bufferline.lua
@@ -1,3 +1,5 @@
 require("bufferline").setup {
-    highlights = require("catppuccin.groups.integrations.bufferline").get()
+    highlights = require("catppuccin.groups.integrations.bufferline").get(),
+    options = {
+    }
 }
