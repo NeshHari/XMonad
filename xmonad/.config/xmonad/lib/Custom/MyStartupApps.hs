8b90a122f7412d4a61f1ce6584636618d7ee2982
Nesh Hari <neshharipil@gmail.com>
2023-01-15 15:32:57 +0800

    removed manual monitor switch

--- xmonad/.config/xmonad/lib/Custom/MyStartupApps.hs
+++ xmonad/.config/xmonad/lib/Custom/MyStartupApps.hs
@@ -5,7 +5,6 @@ import XMonad.Util.SpawnOnce
 
 myStartupHook :: X ()
 myStartupHook = do
---  spawn "~/scripts/screenlayout/switch.sh"
   spawn "feh --bg-scale ~/wallpapers/stains_of_purple.jpg"
   spawn "~/scripts/feh-blur.sh -s; ~/scripts/feh-blur.sh -d"
   spawnOnce "xmodmap ~/.Xmodmap"
