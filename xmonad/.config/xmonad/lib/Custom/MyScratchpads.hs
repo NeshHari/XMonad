bd4a52e3074fbec632950316cad91e32df89f0ea
Nesh Hari <neshharipil@gmail.com>
2023-01-17 15:53:50 +0800

    cleanup

--- xmonad/.config/xmonad/lib/Custom/MyScratchpads.hs
+++ xmonad/.config/xmonad/lib/Custom/MyScratchpads.hs
@@ -11,18 +11,19 @@ myScratchpads =
     NS "glava" spawnGl findGl myCenterSmall
   ]
   where
-    {-
-    To get WM_CLASS of a visible window, run "xprop | grep 'CLASS'" and select the window.
-    appName :: Query StringSource
-    Return the application name; i.e., the first string returned by WM_CLASS.
-
-    resource :: Query StringSource
-    Backwards compatible alias for appName.
-
-    className :: Query StringSource
-    Return the resource class; i.e., the second string returned by WM_CLASS. -}
     spawnQc = "alacritty -e fish"
     findQc = appName =? "Alacritty"
 
     spawnGl = "glava"
     findGl = appName =? "GLava"
+
+{-
+To get WM_CLASS of a visible window, run "xprop | grep 'CLASS'" and select the window.
+appName :: Query String
+Return the application name; i.e., the first string returned by WM_CLASS.
+
+resource :: Query String
+Backwards compatible alias for appName.
+
+className :: Query String
+Return the resource class; i.e., the second string returned by WM_CLASS. -}
