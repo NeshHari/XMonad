347b829af10ff19bbd0bc7d5580994f747b661f4
NeshHari <neshharipil@gmail.com>
2023-01-30 20:06:58 +0800

    new dashboard header

--- nvim/.config/nvim/after/plugin/alpha.lua
+++ nvim/.config/nvim/after/plugin/alpha.lua
@@ -1,26 +1,36 @@
-local dashboard = require('alpha.themes.dashboard')
+local dashboard = require("alpha.themes.dashboard")
 
 -- Banner
-local banner = {
-    "                                                    ",
-    " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                                                    ",
+local nvim = {
+	"                                                    ",
+	" ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+	" ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+	" ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+	" ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+	" ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+	" ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+	"                                                    ",
 }
 
-dashboard.section.header.val = banner
+local darkarts = {
+	"████████╗██╗  ██╗███████╗    ██████╗  █████╗ ██████╗ ██╗  ██╗     █████╗ ██████╗ ████████╗███████╗",
+	"╚══██╔══╝██║  ██║██╔════╝    ██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝    ██╔══██╗██╔══██╗╚══██╔══╝██╔════╝",
+	"   ██║   ███████║█████╗      ██║  ██║███████║██████╔╝█████╔╝     ███████║██████╔╝   ██║   ███████╗",
+	"   ██║   ██╔══██║██╔══╝      ██║  ██║██╔══██║██╔══██╗██╔═██╗     ██╔══██║██╔══██╗   ██║   ╚════██║",
+	"   ██║   ██║  ██║███████╗    ██████╔╝██║  ██║██║  ██║██║  ██╗    ██║  ██║██║  ██║   ██║   ███████║",
+	"   ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝",
+	"                                                                                                  ",
+}
+
+dashboard.section.header.val = darkarts
 
 dashboard.section.buttons.val = {
-    dashboard.button('SPC f f', '  Find File', ':Telescope find_files find_command=rg,--hidden,--files<CR>'),
-    dashboard.button('SPC f b', '  File Browser', ':Telescope file_browser<CR>'),
-    dashboard.button('SPC f w', '  Find Word', ':Telescope live_grep<CR>'),
-    dashboard.button('SPC f d', '  Dotfiles', ':cd $HOME/dotfiles/ | Telescope file_browser<CR>'),
-    dashboard.button('SPC f h', '  Help', ':Telescope help_tags<CR>'),
-    dashboard.button('q', '  Get Me Outta Here', ':qa<CR>'),
+	dashboard.button("SPC f f", "  Find File", ":Telescope find_files find_command=rg,--hidden,--files<CR>"),
+	dashboard.button("SPC f b", "  File Browser", ":Telescope file_browser<CR>"),
+	dashboard.button("SPC f w", "  Find Word", ":Telescope live_grep<CR>"),
+	dashboard.button("SPC f d", "  Dotfiles", ":cd $HOME/dotfiles/ | Telescope file_browser<CR>"),
+	dashboard.button("SPC f h", "  Help", ":Telescope help_tags<CR>"),
+	dashboard.button("q", "  Get Me Outta Here", ":qa<CR>"),
 }
 
-require 'alpha'.setup(dashboard.config)
+require("alpha").setup(dashboard.config)
