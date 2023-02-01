5608858085d03a0eced9df24caf8c7dea1a63c68
NeshHari <neshharipil@gmail.com>
2023-02-01 15:09:18 +0800

    resize from any corner

--- xmonad/.config/xmonad/lib/Custom/MyMouse.hs
+++ xmonad/.config/xmonad/lib/Custom/MyMouse.hs
@@ -1,21 +1,18 @@
 module Custom.MyMouse where
 
-import qualified Data.Map as M
+import Data.Map qualified as M
 import XMonad
-import qualified XMonad.StackSet as W
+import XMonad.Actions.FlexibleResize qualified as Flex
+import XMonad.StackSet qualified as W
 
+myMouseBindings :: XConfig l -> M.Map (KeyMask, Button) (Window -> X ())
 myMouseBindings XConfig {XMonad.modMask = modm} =
   M.fromList
-    -- mod-button1, Set the window to floating mode and move by dragging
     [ ( (modm, button1),
         \w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster
       ),
-      -- mod-button2, Raise the window to the top of the stack
       ((modm, button2), \w -> focus w >> windows W.shiftMaster),
-      -- mod-button3, Set the window to floating mode and resize by dragging
       ( (modm, button3),
-        \w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster
+        \w -> focus w >> Flex.mouseResizeWindow w >> windows W.shiftMaster
       )
-      -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
-
