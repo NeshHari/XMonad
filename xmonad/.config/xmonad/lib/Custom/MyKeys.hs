9ee6707c35a36ebeee759a0effd1e0e4a2c32c63
NeshHari <neshharipil@gmail.com>
2023-02-02 23:20:11 +0800

    jump to monocle

--- xmonad/.config/xmonad/lib/Custom/MyKeys.hs
+++ xmonad/.config/xmonad/lib/Custom/MyKeys.hs
@@ -67,6 +67,7 @@ myKeys =
     ("M-<Space>", sendMessage NextLayout),
     ("M-C-<Space>", spawn "polybar-msg cmd toggle" >> sendMessage ToggleStruts),
     ("M-C-b", sendMessage $ Toggle NOBORDERS),
+    ("M-f", sendMessage $ JumpToLayout "Monocle"),
     -- Cycle workspaces
     ("M-<Down>", nextWS),
     ("M-<Up>", prevWS),
