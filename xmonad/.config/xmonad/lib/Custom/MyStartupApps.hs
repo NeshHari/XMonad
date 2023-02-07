c54ec83aa778acd4f35062bf0ce25c8fd005e2b7
NeshHari <neshharipil@gmail.com>
2023-02-07 20:51:59 +0800

    wait for picom to be killed before restart

--- xmonad/.config/xmonad/lib/Custom/MyStartupApps.hs
+++ xmonad/.config/xmonad/lib/Custom/MyStartupApps.hs
@@ -7,5 +7,5 @@ myStartupHook :: X ()
 myStartupHook = do
   spawn "feh --bg-scale ~/wallpapers/dark_arts_custom.png"
   spawn "~/scripts/feh-blur.sh -s; ~/scripts/feh-blur.sh -d"
-  spawn "killall picom; picom -b"
+  spawn "killall picom; sleep 1 && picom -b"
   spawnOnce "easyeffects --gapplication-service &"
