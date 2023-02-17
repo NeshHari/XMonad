b0060c15c46e9bd937482483174560bdefd65af6
NeshHari <neshharipil@gmail.com>
2023-02-17 16:42:51 +0800

    cleanup

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,131 +1,131 @@
 function fish_greeting
 
-    set quotes \
-	"It does not do to dwell on dreams and forget to live."\
-	"Fear of a name only increases fear of the thing itself."\
-	"Happiness can be found even in the darkest of times, if one only remembers to turn on the light."\
-	"The ones that love us never really leave us."\
-	"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."\
-	"It is not our abilities that show what we truly are, it is our choices."\
-	"We've all got both light and dark inside us. What matters is the part we choose to act on. That's who we really are."\
-	"It is important to fight, and fight again, and keep fighting, for only then can evil be kept at bay, though never quite eradicated."\
-	"The best of us sometimes eat our words."\
-	"Wit beyond measure is man’s greatest treasure."\
-	"Youth cannot know how age thinks and feels. But old men are guilty if they forget what it was to be young."\
-	"We do not need magic to change the world, we carry all the power we need inside ourselves already."\
-	"It is a curious thing, Harry, but perhaps those who are best suited to power are those who have never sought it."\
-	"The scar had not pained Harry for nineteen years. All was well."\
-	"We must all face the choice between what is right and what is easy."\
-	"The truth. It is a beautiful and terrible thing, and must therefore be treated with great caution."\
-	"We're all human, aren't we? Every human life is worth the same, and worth saving."\
-	"To the well-organized mind, death is but the next great adventure."\
-	"Of course it is happening inside your head, Harry, but why on earth should that mean it is not real?"\
-	"Don't let the muggles get you down."\
-	"It is our choices, Harry, that show what we truly are, far more than our abilities."\
-	"There are some things you can't share without ending up liking each other."\
-	"Dark times lie ahead of us and there will be a time when we must choose between what is easy and what is right."\
-	"It takes a great deal of bravery to stand up to your enemies, but a great deal more to stand up to your friends."\
-	"We're all still here. That's something, isn't it?"\
-	"Age is foolish and forgetful when it underestimates youth."\
-	"Differences of habit and language are nothing at all if our aims are identical and our hearts are open."\
-	"We are only as strong as we are united, as weak as we are divided."\
-	"We're only as good as the company we keep."\
-	"Nothing like a nighttime stroll to give you ideas."\
-	"Time will not slow down when something unpleasant lies ahead."\
-	"Life is difficult, my dear. And complicated."\
-	"One can never have enough socks."\
-	"The mind is not a book, to be opened at will and examined at leisure."\
-	"The wand chooses the wizard."\
-	"We do not need magic to transform our world. We carry all the power we need inside ourselves already."\
-	"Numbing the pain for a while will make it worse when you finally feel it."\
-	"Hearing voices no one else can hear isn't a good sign, even in the wizarding world."\
-	"Not all those who wander are lost."\
-	"It is the unknown we fear when we look upon death and darkness, nothing more."\
-	"We have all got both light and dark inside us. What matters is the part we choose to show."\
-	"It is the unknown that terrifies us."\
-	"A thousand wizards can't change fate."\
-	"Wit beyond measure is a man's greatest treasure."\
-	"Age is foolish and forgetful when it underestimates youth."\
-	"The greatest thing you'll ever learn is just to love and be loved in return."\
-	"You will find that help will always be given at Hogwarts to those who ask for it."\
-	"To the well-organized mind, life is but a continuous series of lessons."\
-	"It is our choices that define us."\
-	"In dreams, we enter a world entirely our own."\
-	"Magic always leaves traces."\
-	"Life is not a dress rehearsal."\
-	"It is the quality of one's convictions that determines success."\
-	"Knowledge speaks, but wisdom listens."\
-	"The world is not divided into good people and Death Eaters."\
-	"There is an expiry date on blaming your parents for steering you in the wrong direction."\
-	"To conquer fear is the beginning of wisdom."\
-	"No good sitting worrying about your foot when your hat's come off."\
-	"The ones who love us never really leave us."\
-	"Every story, if it's any good, has been told a thousand times."\
-	"Death is just the next great adventure."\
-	"Fear of a name increases fear of the thing itself."\
-	"Destiny is a name often given in retrospect to choices that had dramatic consequences."\
-	"It does not do to dwell on dreams and forget to live."\
-	"He who lived by the sword, died by the sword."\
-	"The truth. It is a beautiful and terrible thing, and should therefore be treated with great caution."\
-	"Where your treasure is, there your heart will be also."\
-	"The most difficult thing is the decision to act, the rest is merely tenacity."\
-	"Knowledge is a weapon. Arm yourself well before you go into battle."\
-	"A good leader takes a little more than his share of the blame, a little less than his share of the credit."\
-	"It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends."\
-	"The greatest adventure is what lies ahead."\
-	"With great power comes great responsibility."\
-	"It's not what you have in life, it's who you have in your life that counts."\
-	"It takes great courage to see the world in all its tarnished glory and still love it."\
-	"Do not be too proud of this wizardry. I have still to hear of a wizard who could produce fire by rubbing two dry sticks together."\
-	"Fear of a name only increases fear of the thing itself."\
-	"I don't want to be a product of my environment, I want my environment to be a product of me."\
-	"What you know you can't explain, but you feel it. You've felt it your entire life, that there's something wrong with the world."\
-	"Experience is a brutal teacher, but you learn. My God, do you learn."\
-	"There is a room in the Department of Mysteries that is kept locked at all times. It contains a force that is at once more wonderful and more terrible than death, than human intelligence, than the forces of nature."\
-	"We are only as strong as we are united, as weak as we are divided."\
-	"Fear makes the wolf bigger than he is."\
-	"If one advances confidently in the direction of one's dreams and endeavors to live the life which one has imagined, one will meet success unexpected in common hours."\
-	"Once we have tasted far streams, it is hard to be content with a narrow life."\
-	"There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy."\
-	"If you want to see the true measure of a man, watch how he treats his inferiors, not his equals."\
-	"Sometimes you have to let go of what you know to become what you need to be."\
-	"We're all human, aren't we? Every human life is worth the same, and worth saving."\
-	"It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends."\
-	"The only way to get the best of an argument is to avoid it."\
-	"A person who has good thoughts cannot ever be ugly. You can have a wonky nose and a crooked mouth and a double chin and stick-out teeth, but if you have good thoughts they will shine out of your face like sunbeams and you will always look lovely."\
-	"The only thing we have to fear is fear itself."\
-	"It's not what we have in life that determines our worth, it's what we do with what we have."\
-	"Words are, in my not-so-humble opinion, our most inexhaustible source of magic."\
-	"We must all face the choice between what is right and what is easy."\
-	"Words are a powerful weapon, use them wisely."\
-	"We're all in this together, no one can take on the world alone."\
-	"You can't always trust those closest to you, but you have to take that risk, because love is worth it."\
-	"Life is about creating yourself, not finding yourself."\
-	"No good can come of hurting others, only pain."\
-	"The ones who mind don't matter, and the ones who matter don't mind."\
-	"We can face anything as long as we have each other."\
-	"In the end, we will remember not the words of our enemies, but the silence of our friends."\
-    "Books! And cleverness! There are more important things - friendship and bravery." \
-    "I hope you're pleased with yourselves. We could all have been killed - or worse, expelled."\
-    "It's not everyday you meet someone who's proud they're related to you."\
-    "It's LeviOsa, not LeviosAR." \
-    "I don't go looking for trouble. Trouble usually finds me."\
-    "I’m going to keep going until I succeed—or die. Don’t think I don’t know how this might end. I’ve known it for years."\
-    "I won't let you do it. I’ll...I'll fight you!"\
-    "I'm not going to stop the wheel. I'm going to break the wheel."\
-    "Knowledge is power."\
-    "Just because you have the emotional range of a teaspoon doesn't mean we all have."\
-    "We do not need magic to change the world, we carry all the power we need inside ourselves already."\
-    "There's no need to call me sir, Professor."\
-    "I’m Ron Weasley. Have you seen my family around here?"\
-    "Just because it's taken you three years to notice, Ron, doesn't mean no one else has spotted I'm a witch."\
-    "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends."\
-    "I’m not going to be murdered."\
-    "You're a prefect? Oh Ronnie! That's everyone in the family!"\
-    "She needs to sort out her priorities. I’ve been really nice about it so far. I’ve just said she’s a busy person."
-
-	set to_show (random)"%"(count $quotes)
-	set to_show $quotes[(math $to_show"+1")]
-	printf "%s\n" $to_show
+ #    set quotes \
+	# "It does not do to dwell on dreams and forget to live."\
+	# "Fear of a name only increases fear of the thing itself."\
+	# "Happiness can be found even in the darkest of times, if one only remembers to turn on the light."\
+	# "The ones that love us never really leave us."\
+	# "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."\
+	# "It is not our abilities that show what we truly are, it is our choices."\
+	# "We've all got both light and dark inside us. What matters is the part we choose to act on. That's who we really are."\
+	# "It is important to fight, and fight again, and keep fighting, for only then can evil be kept at bay, though never quite eradicated."\
+	# "The best of us sometimes eat our words."\
+	# "Wit beyond measure is man’s greatest treasure."\
+	# "Youth cannot know how age thinks and feels. But old men are guilty if they forget what it was to be young."\
+	# "We do not need magic to change the world, we carry all the power we need inside ourselves already."\
+	# "It is a curious thing, Harry, but perhaps those who are best suited to power are those who have never sought it."\
+	# "The scar had not pained Harry for nineteen years. All was well."\
+	# "We must all face the choice between what is right and what is easy."\
+	# "The truth. It is a beautiful and terrible thing, and must therefore be treated with great caution."\
+	# "We're all human, aren't we? Every human life is worth the same, and worth saving."\
+	# "To the well-organized mind, death is but the next great adventure."\
+	# "Of course it is happening inside your head, Harry, but why on earth should that mean it is not real?"\
+	# "Don't let the muggles get you down."\
+	# "It is our choices, Harry, that show what we truly are, far more than our abilities."\
+	# "There are some things you can't share without ending up liking each other."\
+	# "Dark times lie ahead of us and there will be a time when we must choose between what is easy and what is right."\
+	# "It takes a great deal of bravery to stand up to your enemies, but a great deal more to stand up to your friends."\
+	# "We're all still here. That's something, isn't it?"\
+	# "Age is foolish and forgetful when it underestimates youth."\
+	# "Differences of habit and language are nothing at all if our aims are identical and our hearts are open."\
+	# "We are only as strong as we are united, as weak as we are divided."\
+	# "We're only as good as the company we keep."\
+	# "Nothing like a nighttime stroll to give you ideas."\
+	# "Time will not slow down when something unpleasant lies ahead."\
+	# "Life is difficult, my dear. And complicated."\
+	# "One can never have enough socks."\
+	# "The mind is not a book, to be opened at will and examined at leisure."\
+	# "The wand chooses the wizard."\
+	# "We do not need magic to transform our world. We carry all the power we need inside ourselves already."\
+	# "Numbing the pain for a while will make it worse when you finally feel it."\
+	# "Hearing voices no one else can hear isn't a good sign, even in the wizarding world."\
+	# "Not all those who wander are lost."\
+	# "It is the unknown we fear when we look upon death and darkness, nothing more."\
+	# "We have all got both light and dark inside us. What matters is the part we choose to show."\
+	# "It is the unknown that terrifies us."\
+	# "A thousand wizards can't change fate."\
+	# "Wit beyond measure is a man's greatest treasure."\
+	# "Age is foolish and forgetful when it underestimates youth."\
+	# "The greatest thing you'll ever learn is just to love and be loved in return."\
+	# "You will find that help will always be given at Hogwarts to those who ask for it."\
+	# "To the well-organized mind, life is but a continuous series of lessons."\
+	# "It is our choices that define us."\
+	# "In dreams, we enter a world entirely our own."\
+	# "Magic always leaves traces."\
+	# "Life is not a dress rehearsal."\
+	# "It is the quality of one's convictions that determines success."\
+	# "Knowledge speaks, but wisdom listens."\
+	# "The world is not divided into good people and Death Eaters."\
+	# "There is an expiry date on blaming your parents for steering you in the wrong direction."\
+	# "To conquer fear is the beginning of wisdom."\
+	# "No good sitting worrying about your foot when your hat's come off."\
+	# "The ones who love us never really leave us."\
+	# "Every story, if it's any good, has been told a thousand times."\
+	# "Death is just the next great adventure."\
+	# "Fear of a name increases fear of the thing itself."\
+	# "Destiny is a name often given in retrospect to choices that had dramatic consequences."\
+	# "It does not do to dwell on dreams and forget to live."\
+	# "He who lived by the sword, died by the sword."\
+	# "The truth. It is a beautiful and terrible thing, and should therefore be treated with great caution."\
+	# "Where your treasure is, there your heart will be also."\
+	# "The most difficult thing is the decision to act, the rest is merely tenacity."\
+	# "Knowledge is a weapon. Arm yourself well before you go into battle."\
+	# "A good leader takes a little more than his share of the blame, a little less than his share of the credit."\
+	# "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends."\
+	# "The greatest adventure is what lies ahead."\
+	# "With great power comes great responsibility."\
+	# "It's not what you have in life, it's who you have in your life that counts."\
+	# "It takes great courage to see the world in all its tarnished glory and still love it."\
+	# "Do not be too proud of this wizardry. I have still to hear of a wizard who could produce fire by rubbing two dry sticks together."\
+	# "Fear of a name only increases fear of the thing itself."\
+	# "I don't want to be a product of my environment, I want my environment to be a product of me."\
+	# "What you know you can't explain, but you feel it. You've felt it your entire life, that there's something wrong with the world."\
+	# "Experience is a brutal teacher, but you learn. My God, do you learn."\
+	# "There is a room in the Department of Mysteries that is kept locked at all times. It contains a force that is at once more wonderful and more terrible than death, than human intelligence, than the forces of nature."\
+	# "We are only as strong as we are united, as weak as we are divided."\
+	# "Fear makes the wolf bigger than he is."\
+	# "If one advances confidently in the direction of one's dreams and endeavors to live the life which one has imagined, one will meet success unexpected in common hours."\
+	# "Once we have tasted far streams, it is hard to be content with a narrow life."\
+	# "There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy."\
+	# "If you want to see the true measure of a man, watch how he treats his inferiors, not his equals."\
+	# "Sometimes you have to let go of what you know to become what you need to be."\
+	# "We're all human, aren't we? Every human life is worth the same, and worth saving."\
+	# "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends."\
+	# "The only way to get the best of an argument is to avoid it."\
+	# "A person who has good thoughts cannot ever be ugly. You can have a wonky nose and a crooked mouth and a double chin and stick-out teeth, but if you have good thoughts they will shine out of your face like sunbeams and you will always look lovely."\
+	# "The only thing we have to fear is fear itself."\
+	# "It's not what we have in life that determines our worth, it's what we do with what we have."\
+	# "Words are, in my not-so-humble opinion, our most inexhaustible source of magic."\
+	# "We must all face the choice between what is right and what is easy."\
+	# "Words are a powerful weapon, use them wisely."\
+	# "We're all in this together, no one can take on the world alone."\
+	# "You can't always trust those closest to you, but you have to take that risk, because love is worth it."\
+	# "Life is about creating yourself, not finding yourself."\
+	# "No good can come of hurting others, only pain."\
+	# "The ones who mind don't matter, and the ones who matter don't mind."\
+	# "We can face anything as long as we have each other."\
+	# "In the end, we will remember not the words of our enemies, but the silence of our friends."\
+ #    "Books! And cleverness! There are more important things - friendship and bravery." \
+ #    "I hope you're pleased with yourselves. We could all have been killed - or worse, expelled."\
+ #    "It's not everyday you meet someone who's proud they're related to you."\
+ #    "It's LeviOsa, not LeviosAR." \
+ #    "I don't go looking for trouble. Trouble usually finds me."\
+ #    "I’m going to keep going until I succeed—or die. Don’t think I don’t know how this might end. I’ve known it for years."\
+ #    "I won't let you do it. I’ll...I'll fight you!"\
+ #    "I'm not going to stop the wheel. I'm going to break the wheel."\
+ #    "Knowledge is power."\
+ #    "Just because you have the emotional range of a teaspoon doesn't mean we all have."\
+ #    "We do not need magic to change the world, we carry all the power we need inside ourselves already."\
+ #    "There's no need to call me sir, Professor."\
+ #    "I’m Ron Weasley. Have you seen my family around here?"\
+ #    "Just because it's taken you three years to notice, Ron, doesn't mean no one else has spotted I'm a witch."\
+ #    "It takes a great deal of bravery to stand up to our enemies, but just as much to stand up to our friends."\
+ #    "I’m not going to be murdered."\
+ #    "You're a prefect? Oh Ronnie! That's everyone in the family!"\
+ #    "She needs to sort out her priorities. I’ve been really nice about it so far. I’ve just said she’s a busy person."
+	#
+	# set to_show (random)"%"(count $quotes)
+	# set to_show $quotes[(math $to_show"+1")]
+	# printf "%s\n" $to_show
 
 end
