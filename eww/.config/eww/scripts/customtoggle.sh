03beaf4c34e93b6507d7f83bd7ea961c74cd5d9d
NeshHari <neshharipil@gmail.com>
2023-02-22 14:56:19 +0800

    refinements...

--- eww/.config/eww/scripts/customtoggle.sh
+++ eww/.config/eww/scripts/customtoggle.sh
@@ -9,6 +9,11 @@ module="$1"
 
 # Check if the module is valid
 if [[ " ${modules[@]} " =~ " ${module} " ]]; then
+	# Check if the module is already open, and if so, close it
+	if eww query -m "$module" >/dev/null 2>&1; then
+		eww close "$module"
+	fi
+
 	# Close all modules
 	for mod in "${modules[@]}"; do
 		if [[ "$mod" != "$module" ]]; then
--- eww/.config/eww/scripts/startup.sh
+++ eww/.config/eww/scripts/startup.sh
@@ -4,4 +4,5 @@ killall -9 eww
 rm ~/.cache/eww*
 eww daemon
 eww open bar
-xdo lower -n bar
+sleep 3
+xdo lower -n eww-bar
