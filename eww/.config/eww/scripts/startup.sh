3f9fe5fc5af4ea75c0b8d04ec73a9b4974600c84
NeshHari <neshharipil@gmail.com>
2023-02-24 01:01:12 +0800

    more reliable notification detection

--- eww/.config/eww/scripts/startup.sh
+++ eww/.config/eww/scripts/startup.sh
@@ -4,8 +4,13 @@ modules=("bar" "showvolume" "quotetoggler" "mynotifications")
 
 killall -9 eww
 rm ~/.cache/eww*
+pkill -f "logger.py init"
 
 eww daemon
+sleep 1
+
+cd ~/.config/eww && python ./scripts/logger.py init &
+
 for module in "${modules[@]}"; do
 	eww open "$module"
 done
