81201eadeba9244f68c63022b5b6062ebffeabb7
NeshHari <neshharipil@gmail.com>
2023-02-24 00:31:53 +0800

    multi-select AUR packages update via rofi

--- eww/.config/eww/scripts/update.sh
+++ eww/.config/eww/scripts/update.sh
@@ -1,2 +1,20 @@
 #!/usr/bin/env bash
-alacritty -e "paru"
+# Author: github.com/NeshHari
+
+packages=$(paru -Qu | cut -d' ' -f1)
+
+update_all_option="Update all"
+selected_packages=$(echo -e "$update_all_option\n${packages}" | rofi -dmenu -i -multi-select -markup-rows -matching fuzzy -p "Select packages to update:")
+
+if [ -z "${selected_packages}" ]; then
+	exit 0
+fi
+
+if [[ "$selected_packages" == *"$update_all_option"* ]]; then
+	update_cmd="paru -S --noconfirm $(echo ${packages})"
+else
+	update_cmd="paru -S --noconfirm $(echo ${selected_packages})"
+fi
+
+echo "Updating packages: ${selected_packages}"
+alacritty -e fish -c "${update_cmd}"
