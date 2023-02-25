b379ef943acedfcd95ed781de9c6d16da28aa91c
NeshHari <neshharipil@gmail.com>
2023-02-25 16:25:39 +0800

    more sensible UI when no updates pending

--- eww/.config/eww/scripts/update.sh
+++ eww/.config/eww/scripts/update.sh
@@ -3,14 +3,20 @@
 
 packages=$(paru -Qu | cut -d' ' -f1)
 
-update_all_option="Update all"
-selected_packages=$(echo -e "$update_all_option\n${packages}" | rofi -dmenu -i -multi-select -markup-rows -matching fuzzy -p "Select packages to update:")
+if [ -z "$packages" ]; then
+	selected_packages=$(echo -e "Exit" | rofi -dmenu -i -markup-rows -matching fuzzy -p "No packages to update:")
+else
+	update_all_option="Update all"
+	selected_packages=$(echo -e "$update_all_option\n${packages}" | rofi -dmenu -i -multi-select -markup-rows -matching fuzzy -p "Select packages to update:")
+fi
 
 if [ -z "${selected_packages}" ]; then
 	exit 0
 fi
 
-if [[ "$selected_packages" == *"$update_all_option"* ]]; then
+if [[ "$selected_packages" == "close" ]]; then
+	exit 0
+elif [[ "$selected_packages" == *"$update_all_option"* ]]; then
 	# update_cmd="paru -S --noconfirm $(echo ${packages})"
 	update_cmd="paru -S $(echo ${packages})"
 else
