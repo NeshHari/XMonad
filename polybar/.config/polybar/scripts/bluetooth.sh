444144e3277f14904f35bff49cf689fc1430ea3b
NeshHari <neshharipil@gmail.com>
2023-03-23 14:17:40 +0800

    removed polybar

--- polybar/.config/polybar/scripts/bluetooth.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if bluetoothctl show | grep -q "Powered: yes" && bluetoothctl info | grep -q 'Device'; then
-	echo "󰂰"
-else
-	echo "󰂲"
-fi
--- polybar/.config/polybar/scripts/poly_spoti_readme.md
+++
@@ -1,1 +0,0 @@
-Note: To work with app launchers, change the /.local/share/applications/spotify.desktop "Exec" line to the path of the poly_spoti.sh script
